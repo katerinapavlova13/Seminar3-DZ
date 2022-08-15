9f66501d83d2065b177d516975d8dd2f6fa5b308
Katerina Pavlova <malinakate18@gmail.com>
2022-08-15 16:29:47 +0400

    DZ

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -4,8 +4,20 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
-
-Console.Write("Введите пятизначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if ()
+Console.WriteLine("Введите пятизначное число:");
+string N = Console.ReadLine();
+int flipN = 0;
+for (int i = 0; i < N.Length / 2; i++)
+{ 
+    if (N.Substring(i, 1) != (N.Substring(N.Length - 1 - i, 1)))
+    {
+        Console.WriteLine("Число не является палиндромом");
+        return;
+    }
+    else
+        flipN = 1;
+}
+if (flipN == 1) 
+{
+    Console.WriteLine("Число является палиндромом");
+}
